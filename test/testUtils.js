7d47e19ce6249ae9a201b834051d4aa271b37452
Laura Jackson <playeronereadystart@gmail.com>
2019-01-09 10:56:44 +0000

    add test function for use in all test files

---
+++ test/testUtils.js
@@ -0,0 +1,11 @@
+/**
+ * Returns node(s) with the given selector attribute
+ * @params {ShallowWrapper} wrapper - Enzyme shallow wrapper
+ * @params {String} val - Value of selector attribute for search
+ * @params {String} selector - optional, default set to data-test
+ * @returns {ShallowWrapper}
+ */
+
+export const findByTestAttr = (wrapper, val, selector='data-test') {
+  return wrapper.find(`[${selector}=${val}]`);
+}
