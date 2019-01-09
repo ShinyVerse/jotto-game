c698d66ff0a44d7cc05966bf2b298f0f60b274cd
Laura Jackson <playeronereadystart@gmail.com>
2019-01-09 12:08:51 +0000

    Add testutils for proptypes, include that test in congrats

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from 'check-prop-types';
+
 /**
  * Returns node(s) with the given selector attribute
  * @params {ShallowWrapper} wrapper - Enzyme shallow wrapper
@@ -9,3 +11,12 @@
 export const findByTestAttr = (wrapper, val, selector ='data-test') => {
   return wrapper.find(`[${selector}='${val}']`);
 };
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name);
+    expect(propError).toBeUndefined();
+}
