88753a35fa00207cd135c49f48ddf886920eff8d
Laura Jackson <playeronereadystart@gmail.com>
2019-01-14 14:54:55 +0000

    Add redux-thunk and code up middlewares

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,16 +1,18 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux'; 
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 /**
  * create a testing store with imported reducers, middleware and initial this.state.
- * globals: rootReducer
+ * globals: rootReducer, middlewares
  * @param {Object} initialState - initial state for store
  * @function storeFactory
  * @returns {Store} - Redux store
  */
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
 
 /**
