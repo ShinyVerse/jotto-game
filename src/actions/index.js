985508abce9ff227d758490008354782151a91fd
Laura Jackson <playeronereadystart@gmail.com>
2019-01-14 17:12:58 +0000

    Remove unnecessary test file and update actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,15 @@
 export const actionTypes = {
-  CORRECT_GUESS: 'CORRECT_GUESS'
+  CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD'
 };
+/**
+ * Returns Redux Think function that dispatches GUESS_WORD action
+ * and (conditionally) CORRECT_GUESS action
+ * @param  {string} guessedWord
+ * @return {function} redux thunk function
+ */
+export const guessWord = (guessedWord) => {
+  return function(dispatch, getState) {
 
-export function correctGuess() {
-  return { type: actionTypes.CORRECT_GUESS};
-}
+  };
+};
