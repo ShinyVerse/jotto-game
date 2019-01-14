5dc58f7ef1151445cc3f09bb5045bde1090d6f2b
Laura Jackson <playeronereadystart@gmail.com>
2019-01-14 17:14:03 +0000

    Setup for integration test and shell of guessedWordsReducer

---
+++ src/integration.test.js
@@ -0,0 +1,21 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    test('updates state correctly or unsuccessful guess', () => {
+
+    });
+    test('updates state correctly or successful guess', () => {
+
+    });
+  });
+  describe('some guessed words', () => {
+    test('updates state correctly or unsuccessful guess', () => {
+
+    });
+    test('updates state correctly or successful guess', () => {
+
+    });
+  });
+});
---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,11 @@
+//tests in integration.js
+
+/**
+ * @function guessedWordsReducer
+ * @param  {array} state - Array of guessed words
+ * @param  {object} action - action to be reduced
+ * @return {array} - new guessedWords state
+ */
+export default (state, action) => {
+  return null;
+}
