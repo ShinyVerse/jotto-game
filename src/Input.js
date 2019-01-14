a74bf74d17a549bd6aebc13e83c3f73f8eca856f
Laura Jackson <playeronereadystart@gmail.com>
2019-01-14 12:13:08 +0000

    Add code and test for Input for success:false

--- src/Input.js
+++ src/Input.js
@@ -3,16 +3,36 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
+    const contents = this.props.success
+      ? null : (
+        <form className='form-inline'>
+          <label>
+            Enter Guess
+            <input
+              data-test='input-box'
+              className='mb-2 mx-sml-3'
+              id='word-guess'
+              type='text'
+              />
+          </label>
+          <button
+            data-test='submit-button'
+            className='btn btn-primary mb-r'
+            type='submit'>
+            Submit
+          </button>
+        </form>
+      )
     return (
-      <div>
-      <button />
+      <div data-test='component-input'>
+        { contents }
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 }
 
 export default connect(mapStateToProps)(Input);
